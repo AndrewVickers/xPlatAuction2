8e51b55914581cddc2e4806c3bbb8621d775d62f
Andrew Vickers <dev@andrewvickers.net>
2018-12-09 17:05:15 +0000

    Added swagger

---
+++ xPlatAuction2.Backend/App_Start/Swagger.Startup.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Http;
+using System.Web.Http.Description;
+using Microsoft.Azure.Mobile.Server;
+using Microsoft.Azure.Mobile.Server.Swagger;
+using Swashbuckle.Application;
+
+namespace xPlatAuction2.Backend
+{
+	public partial class Startup
+	{
+	    public static void ConfigureSwagger(HttpConfiguration config)
+	    {
+			config.Services.Replace(typeof(IApiExplorer), new MobileAppApiExplorer(config));
+			config.EnableSwagger(c =>
+			{
+			    c.SingleApiVersion("v1", "myService");
+
+				c.OperationFilter<MobileAppHeaderFilter>();
+				c.SchemaFilter<MobileAppSchemaFilter>();
+			})
+			    .EnableSwaggerUi();
+	    }
+	}
+}
---
+++ xPlatAuction2.Backend/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(xPlatAuction2.Backend.Startup))]
+
+namespace xPlatAuction2.Backend
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureMobileApp(app);
+        }
+    }
+}
